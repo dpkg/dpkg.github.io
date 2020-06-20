8d9939511d3a4008676ed4dcb2556440c51ff11b
Giri, Deepak <dgiri@cvent.com>
2020-06-20 20:31:34 +0530

    remove docs from preset config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,14 +76,6 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
-        docs: {
-          // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'doc1',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
